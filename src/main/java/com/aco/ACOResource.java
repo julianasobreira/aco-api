35f92bd2f8847043062af1d41f56d17bf215d3b4
Juliana Bacelar <juliana.univasf@gmail.com>
2018-03-21 12:29:08 -0300

    Clean code

--- src/main/java/com/aco/ACOResource.java
+++ src/main/java/com/aco/ACOResource.java
@@ -18,7 +18,6 @@ import javax.ws.rs.core.Response;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.UriInfo;
 import javax.ws.rs.core.Context;
-import javax.ws.rs.core.GenericEntity;
 
 import java.util.*;
 import java.net.URI;
